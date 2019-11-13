d1b4ce32dd8234462cc99ef45aacaa9a4af01211
malabo1990 <malabo1990@gmail.com>
2019-11-13 23:02:34 +0100

    Add files via upload

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Casimiro Ondo Obiang casimiro.ondo.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+    
+        //Ejercicio: 02201-TallaZapatoConsola
+        final int ANY_LLEGADA_LUNA = 1969;
+        System.out.println("El hombre llegó a la luna en "+ANY_LLEGADA_LUNA);
+    }
+}
